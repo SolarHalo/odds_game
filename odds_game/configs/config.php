1f1d15e9633e454819e09ffe1acbf715430b166d
affiliate <21850845@qq.com>
2013-02-04 17:44:11 +0800

    11

--- odds_game/configs/config.php
+++ odds_game/configs/config.php
@@ -4,7 +4,7 @@
  * Enter description here ...
  * @var unknown_type
  */
-define('DB_NAME', 'odds_game'); 
+define('DB_NAME', 'ibo'); 
 define('DB_USER', 'root'); 
 define('DB_PASSWORD', 'root'); 
 define('DB_HOST', 'localhost'); 
@@ -21,12 +21,12 @@ define("ARRAY_A", "array_a");
 define("ARRAY_N", "array_n");
 
 // 手动指定Smarty的libs目录位置
-define('SMARTY_DIR', 'G:/odds_game/odds_game/lib/Smarty-3.1.13/libs/');
+define('SMARTY_DIR', 'D:/gitspace/odds_game/odds_game/lib/Smarty-3.1.13/libs/');
 
 ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.SMARTY_DIR);
 
 //配置程序根目录,非文档根目录
-define('BASE_HOME', "G:/odds_game/odds_game/");
+define('BASE_HOME', "D:/gitspace/odds_game/odds_game/");
 //获得http路径的地址
 //define('DOMAIN_HOME', $_SERVER['HTTP_HOST']);
 define('DOMAIN_HOME', "http://localhost/");

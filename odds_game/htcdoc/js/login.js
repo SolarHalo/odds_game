32d444578cd48bdf4a6d599f8d903bd9c8b1b2a0
ryanhoho <damon_ho@qq.com>
2013-03-04 22:46:25 +0800

    新浪微博登录

--- odds_game/htcdoc/js/login.js
+++ odds_game/htcdoc/js/login.js
@@ -14,3 +14,7 @@ function bsubmit(){
 	}
 	return true;
 }
+
+function sinaLogin(){
+	window.open('https://api.weibo.com/oauth2/authorize?client_id=995123813&response_type=code&redirect_uri=http://www.ibo123.com/odds_game/htcdoc/sinalogin.php','','height=500,width=611,scrollbars=yes,status =yes');
+}

fef5f4e5104fd8df1777470e4a3673577c45c67a
ryanhoho <damon_ho@qq.com>
2013-03-05 00:11:01 +0800

    新浪微博登录

--- odds_game/htcdoc/sinalogin.php
+++ odds_game/htcdoc/sinalogin.php
@@ -15,8 +15,13 @@ if (isset($_REQUEST['code'])) {
 }
 print_r($token);
 if ($token) {
-	print_r($token);
+	$c = new SaeTClientV2( WB_AKEY , WB_SKEY , $token['access_token'] );
+	$ms  = $c->home_timeline(); // done
+	$uid_get = $c->get_uid();
+	$uid = $uid_get['uid'];
+	$user_message = $c->show_user_by_id( $uid);
+	print_r($user_message);
 }else{
-	
+	echo "ddddddddddddddddddddddd";
 }
 ?>

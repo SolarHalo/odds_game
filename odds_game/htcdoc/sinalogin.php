589013e7f7c3da557266acd5a66e842377ede3ed
ryanhoho <damon_ho@qq.com>
2013-03-04 23:18:42 +0800

    新浪微博登录

--- odds_game/htcdoc/sinalogin.php
+++ odds_game/htcdoc/sinalogin.php
@@ -1,3 +1,7 @@
 <?php
-print_r($_GET);
+$code = $_GET['code'];
+
+header("client_id: 995123813");
+header("client_secret: 9c0a16dd718ee29b2278342230e6ed2a");
+header("Location: https://api.weibo.com/oauth2/access_token?grant_type=".$code."&redirect_uri=http://www.ibo123.com/odds_game/htcdoc/sinaloginsuccess.php&code=CODE");
 ?>

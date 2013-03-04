705d8898e34dc737ce0079c79442d39720819f1a
ryanhoho <damon_ho@qq.com>
2013-03-05 00:04:34 +0800

    新浪微博登录

--- odds_game/htcdoc/login.php
+++ odds_game/htcdoc/login.php
@@ -1,7 +1,7 @@
 <?php
 include "../configs/load.php";
 include BASE_HOME."includes/IboUser.class.php";
-
+include BASE_HOME.'includes/saetv2.ex.class.php';
 
 $smarty->assign("urlroot" , urlroot);
 if(array_key_exists("email", $_GET) && array_key_exists("password", $_GET)){
@@ -24,8 +24,18 @@ if(array_key_exists("email", $_GET) && array_key_exists("password", $_GET)){
 	}
 	
 }else{
+	define( "WB_AKEY" , '995123813' );
+	define( "WB_SKEY" , '9c0a16dd718ee29b2278342230e6ed2a' );
+	define( "WB_CALLBACK_URL" , DOMAIN_HOME.'/sinalogin.php' );
+
+	$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+
+	$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
+	
+	
 	$smarty->assign("loginInfo", "");
 	$smarty->assign("logintem", "loginForm.tpl");
+	$smarty->assign("sinaauthurl", $code_url);
 }
 
 $smarty->display("login.tpl");
--- odds_game/htcdoc/sinalogin.php
+++ odds_game/htcdoc/sinalogin.php
@@ -1,8 +1,22 @@
 <?php
-$code = $_GET['code'];
-$userinfo = "client_id=995123813&client_secret=9c0a16dd718ee29b2278342230e6ed2a";
-header('Authorization: ' . 'Basic ' . base64_encode($userInfo));
-//header("client_id: 995123813");
-//header("client_secret: 9c0a16dd718ee29b2278342230e6ed2a");
-header("Location: https://api.weibo.com/oauth2/access_token?grant_type=".$code."&redirect_uri=http://www.ibo123.com/odds_game/htcdoc/sinaloginsuccess.php&code=CODE");
+include "../configs/load.php";
+include BASE_HOME.'includes/saetv2.ex.class.php';
+
+$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+
+if (isset($_REQUEST['code'])) {
+	$keys = array();
+	$keys['code'] = $_REQUEST['code'];
+	$keys['redirect_uri'] = WB_CALLBACK_URL;
+	try {
+		$token = $o->getAccessToken( 'code', $keys ) ;
+	} catch (OAuthException $e) {
+	}
+}
+print_r($token);
+if ($token) {
+	print_r($token);
+}else{
+	
+}
 ?>
